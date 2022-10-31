6ab3121d6566e22d5c2188923772eb1428566bb3
比蹦 <SLMSkret1o146854@gmail.com>
2022-10-31 17:42:58 +0800

    Rename: ChatColor to ColorManager, Removed: method from net.md_5.bungee.api.ChatColor

---
+++ src/main/java/pers/bibong/autofishing/utils/ColorManager.java
@@ -0,0 +1,53 @@
+package pers.bibong.autofishing.utils;
+
+import net.md_5.bungee.api.ChatColor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ColorManager {
+
+
+    public static @NotNull String color (String mainString) {
+        mainString = forMessageToRGB(mainString);
+
+        if (mainString.contains("&")) {
+            return ChatColor.translateAlternateColorCodes('&', mainString);
+        }
+
+        return mainString;
+    }
+
+    public static @NotNull String replace (@NotNull String mainString, String checkString, String replaceString) {
+        if (mainString.contains(checkString)) {
+            return mainString.replace(checkString, replaceString);
+        }
+        return mainString;
+    }
+
+    /**
+     * 將字串RGB解析成RGB物件
+     *
+     * @param msg 十六進位RGB字串 (<#FF0000> Hello world !)
+     *
+     * @return RGB字串
+     */
+    public static String forMessageToRGB (final @NotNull String msg) {
+        String message = msg;
+
+        final Pattern hexPattern = Pattern.compile("<#([A-Fa-f0-9]){6}>");
+        Matcher       matcher    = hexPattern.matcher(message);
+
+        while (matcher.find()) {
+            final ChatColor hexColor = ChatColor.of(matcher.group().substring(1, matcher.group().length() - 1));
+            final String       before   = message.substring(0, matcher.start());
+            final String after  = message.substring(matcher.end());
+            message = before + hexColor + after;
+            matcher = hexPattern.matcher(message);
+        }
+
+        return message;
+    }
+
+}
--- src/main/java/pers/bibong/autofishing/utils/Messages.java
+++ src/main/java/pers/bibong/autofishing/utils/Messages.java
@@ -11,39 +11,39 @@ public class Messages {
     public static @NotNull String command_Reload () {
         String prefix = AutoFishing.config.get().getString("Prefix");
         String reload = AutoFishing.config.get().getString("CommandMessage.Reload");
-        return ChatColor.color(prefix + reload);
+        return ColorManager.color(prefix + reload);
     }
 
     public static @NotNull String command_NoPermission () {
         String prefix = AutoFishing.config.get().getString("Prefix");
         String reload = AutoFishing.config.get().getString("CommandMessage.NoPermission");
-        return ChatColor.color(prefix + reload);
+        return ColorManager.color(prefix + reload);
     }
 
     public static @NotNull String command_NotPlayer () {
         String prefix = AutoFishing.config.get().getString("Prefix");
         String reload = AutoFishing.config.get().getString("CommandMessage.NotPlayer");
-        return ChatColor.color(prefix + reload);
+        return ColorManager.color(prefix + reload);
     }
 
     public static @NotNull String command_Enabled_AutoFishing () {
         String prefix = AutoFishing.config.get().getString("Prefix");
         String reload = AutoFishing.config.get().getString("CommandMessage.Enabled_AutoFishing");
-        return ChatColor.color(prefix + reload);
+        return ColorManager.color(prefix + reload);
     }
 
     public static @NotNull String command_Disabled_AutoFishing () {
         String prefix = AutoFishing.config.get().getString("Prefix");
         String reload = AutoFishing.config.get().getString("CommandMessage.Disabled_AutoFishing");
-        return ChatColor.color(prefix + reload);
+        return ColorManager.color(prefix + reload);
     }
 
     public static void sendMsgToConsole (@NotNull JavaPlugin plugin, String s) {
-        Bukkit.getConsoleSender().sendMessage(ChatColor.color("&a[&6" + plugin.getName() + "&a]&r " + s));
+        Bukkit.getConsoleSender().sendMessage(ColorManager.color("&a[&6" + plugin.getName() + "&a]&r " + s));
     }
 
     public static void sendMsgToPlayer (@NotNull CommandSender sender, String s) {
-        sender.sendMessage(ChatColor.color(s));
+        sender.sendMessage(ColorManager.color(s));
     }
 
 }
